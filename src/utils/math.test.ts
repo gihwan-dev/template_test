252e80f2ac5be0cb86d431aa6a7c959267df6eed
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-04-10 19:08:59 +0900

    feat(test): 테스트 추가

--- src/utils/math.test.ts
+++ src/utils/math.test.ts
@@ -4,3 +4,7 @@ import { sum } from './math.utils.ts';
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(sum(2, 2)).toBe(4);
+});
